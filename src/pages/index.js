20dd6b94d4158bc9e0d6923e939a3ad05ddb7f22
punyamsingh <89277920+punyamsingh@users.noreply.github.com>
2023-12-30 21:06:14 +0530

    made the basic functionality work

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,53 @@
+// index.js
+
+import { useState } from 'react';  // Import the useState hook
 import Head from 'next/head';
 import Image from 'next/image';
-import { Inter } from 'next/font/google';
 import styles from '@/styles/Home.module.css';
-
-const inter = Inter({ subsets: ['latin'] });
+import supabase from '@/sb/supabase'; // Update the path to your supabase.js file
 
 export default function Home() {
+
+  const [text,setText] = useState('');  // Add a state variable for the textarea value
+
+  // const handleSave = async (text) => {
+  //   // Save text to Supabase
+  //   const { data,error } = await supabase
+  //     .from('snippets')
+  //     .insert([{ text },])
+  //     .select();
+
+  //   if (error) {
+  //     console.error('Error saving to Supabase:',error.message);
+  //     return;
+  //   }
+
+  //   // Redirect to the stored page with the unique id
+  //   const storedId = data[0].id;
+  //   window.location.href = `/stored/${storedId}`;
+  // };
+
+  const handleSave = async () => {
+    try {
+      // Save text to Supabase
+      const { data,error } = await supabase
+        .from('snippets')
+        .insert([{ text },])
+        .select();
+
+      if (error) {
+        console.error('Error saving to Supabase:',error.message);
+        return;
+      }
+
+      // Redirect to the stored page with the unique id
+      const storedId = data[0].id;
+      window.location.href = `/stored/${storedId}`;
+    } catch (error) {
+      console.error('Unhandled error:',error);
+    }
+  };
+
   return (
     <>
       <Head>
@@ -14,7 +56,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/apple-touch-icon.png" />
       </Head>
-      <main className={`${styles.main} ${inter.className}`}>
+      <main className={styles.main}>
         <div className={styles.center}>
           <Image
             className={styles.logo}
@@ -29,8 +71,12 @@ export default function Home() {
           <textarea
             className={styles.textarea}
             placeholder="Paste your code here..."
+            value={text}
+            onChange={(e) => setText(e.target.value)}  // Update the state on change
           ></textarea>
-          <button className={styles.button}>Submit</button>
+          <button className={styles.button} onClick={handleSave}>
+            Save
+          </button>
         </div>
       </main>
     </>
---
+++ src/pages/stored/[id].js
@@ -0,0 +1,58 @@
+// pages/stored/[id].js
+
+import { useEffect,useState } from 'react';
+import Router from 'next/router';
+import TopLoadingBar from 'react-top-loading-bar';
+import supabase from '@/sb/supabase'; // Update the path to your supabase.js file
+
+export default function StoredPage({ storedText }) {
+    const [text,setText] = useState(storedText);
+    const [loadingBar,setLoadingBar] = useState(0);
+
+    useEffect(() => {
+        // Fetch stored text based on the unique ID
+        const fetchStoredText = async () => {
+            try {
+                setLoadingBar(30); // Start loading bar
+                const storedId = Router.query.id; // Use Router.query to get the ID
+                const { data,error } = await supabase
+                    .from('snippets')
+                    .select('text')
+                    .eq('id',storedId)
+                    .single();
+
+                if (error) {
+                    console.error('Error fetching stored text:',error.message);
+                    return;
+                }
+
+                setText(data.text);
+                setLoadingBar(100); // Complete loading bar
+            } catch (error) {
+                console.error('Unhandled error:',error);
+                setLoadingBar(100); // Ensure loading bar completes in case of an error
+            }
+        };
+
+        fetchStoredText();
+    },[]); // Ensure it runs only once on component mount
+
+    return (
+        <div>
+            <TopLoadingBar progress={loadingBar} color="#0071ff" />
+            <h1>Stored Page</h1>
+            <pre>{text}</pre>
+        </div>
+    );
+}
+
+export async function getServerSideProps({ params }) {
+    // No need to fetch stored text here, it's handled in useEffect
+    // ...
+
+    return {
+        props: {
+            storedText: '', // Set initial value, actual value is fetched in useEffect
+        },
+    };
+}
